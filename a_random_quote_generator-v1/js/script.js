fed81ec3633c3232d361ff3def4977d2b855131d
aman-kapoor <54988745+aman-kapoor@users.noreply.github.com>
2021-03-07 01:38:06 +0500

    Added Quotes

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,7 +11,13 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-
+const quotes = [
+  {quote: 'Weeks of coding can save you hours of planning.', source: 'Unknown',},
+  {quote: 'Any application that can be written in JavaScript, will eventually be written in JavaScript',source: 'Jeff Atwood', citation: 'codinghorror', year: 2007 },
+  {quote: 'Machines take me by surprise with great frequency.', source: 'Alan Turing',},
+  {quote: 'Good code is its own best documentation. As you\'re about to add a comment, ask yourself, \'How can I improve the code so that this comment isn\'t needed?\' Improve the code and then document it to make it even clearer.', source: 'Steve McConnell',},
+  {quote: 'To me programming is more than an important practical art. It is also a gigantic undertaking in the foundations of knowledge.', source: 'Grace Hopper',}
+];
 
 /***
  * `getRandomQuote` function
